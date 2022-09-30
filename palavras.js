cbb16cb4a95e3e2b215fc67a034c21f91bbbb776
Leandros10 <111618247+Leandros10@users.noreply.github.com>
2022-09-30 20:42:30 -0300

    Add files via upload
    #challengeonejogoforca3

---
+++ palavras.js
@@ -0,0 +1,35 @@
+let listaPalavras = [
+   
+    ["FUTSAL", "ESPORTE"],
+    ["BASQUETE", "ESPORTE"],
+    ["HANDEBOL", "ESPORTE"],
+    ["FUTEBOL", "ESPORTE"],
+    ["ESGRIMA", "ESPORTE"],
+    ["JUDO", "ESPORTE"],
+    ["HANDEBOL", "ESPORTE"],
+    ["TENIS", "ESPORTE"],
+
+    ["CANETA", "OBJETO"],
+    ["MESA", "OBJETO"],
+    ["MICROONDAS", "OBJETO"],
+    ["VENTILADOR", "OBJETO"],
+    ["TAPETE", "OBJETO"],
+    ["PORTA", "OBJETO"],
+    ["PRATELEIRA", "OBJETO"],
+    ["COLHER", "OBJETO"],
+    ["CAFETEIRA", "OBJETO"],
+    ["VARAL", "OBJETO"],
+    ["CORTINA", "OBJETO"],
+    ["VASSOURA", "OBJETO"],
+    ["AIRFRAYER", "OBJETO"],
+    ["CELULAR", "OBJETO"],
+    ["SMARTPHONE", "OBJETO"],
+    ["GARFO", "OBJETO"],
+    ["CHINELO", "OBJETO"],
+    ["AZULEJO", "OBJETO"],
+    ["PAPIRO", "OBJETO"],
+
+    ["IRLANDA","LUGARES"]
+    ["EQUADOR","LUGARES"]
+    
+];
